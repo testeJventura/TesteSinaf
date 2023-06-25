8173d29192ca636bf644878a43dbef5dff1d6ee6
Jonathan Ventura <jonathan.venturarj@gmail.com>
2023-06-25 16:24:34 -0300

    git commit -m ":tada: Commit inicial"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { AutorizadoFalseComponent } from './pages/autorizado-false/autorizado-false.component';
+import { autorizadoGuard } from './guards/autorizado.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path: '', component: HomeComponent,
+  children:[
+    {path: '', component: Page1Component},
+    {path: 'page', component: Page1Component},
+    
+
+  ], canActivate: [autorizadoGuard]
+},
+  {path: '', component: AutorizadoFalseComponent,
+  children:[
+    {path: '', component: LoginComponent},
+    {path: 'login', component: LoginComponent},
+
+  ]},
+
+    
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { AutorizadoFalseComponent } from './pages/autorizado-false/autorizado-false.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    HomeComponent,
+    Page1Component,
+    AutorizadoFalseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
